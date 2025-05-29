d37c25d5d66aee2f9e3f6b63df4128daa986e894
DaoQuocViet18 <daoquocviet18062003@gmail.com>
2025-05-29 11:48:30 +0700

    Setup_1

---
+++ Assets/_Modules/Game Play/Hole/HoleInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HoleInput : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Chuột trái
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    Debug.Log("Người chơi đã nhấn vào goal!");
+                }
+            }
+        }
+    }
+}
